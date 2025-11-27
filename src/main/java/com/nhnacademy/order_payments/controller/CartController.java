995426f6a38a9f05d883d8a6c2406c39c5b26aad
eugene <hash_ff0000@naver.com>
2025-11-27 17:52:08 +0900

    그냥 저장

---
+++ src/main/java/com/nhnacademy/order_payments/controller/CartController.java
@@ -0,0 +1,65 @@
+package com.nhnacademy.order_payments.controller;
+
+import com.nhnacademy.order_payments.dto.BookInfo;
+import com.nhnacademy.order_payments.service.MemberCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+    private final MemberCartService memberCartService;
+    public CartController(MemberCartService memberCartService) {
+        this.memberCartService = memberCartService;
+    }
+
+    // 장바구니 추가
+    @PostMapping
+    public ResponseEntity<Void> addCartBook(@RequestParam Long bookId,
+                                            @RequestParam Integer quantity,
+                                            @RequestHeader("X-Member-ID") Long userId) {
+        memberCartService.addCartItem(userId, bookId, quantity);
+        return ResponseEntity.ok().build();
+    }
+
+    //    // 장바구니 전체 조회
+    @GetMapping
+    public ResponseEntity<List<BookInfo>> getCartBookList(@RequestHeader("X-Member-ID") Long userId) {
+
+        List<BookInfo> bookList = memberCartService.getCartItemList(userId);
+        if(bookList.isEmpty()) {
+            log.info("장바구니가 비어있습니다.");
+        }
+
+        return ResponseEntity.ok().body(bookList);
+    }
+
+    // 장바구니 특정 책 조회
+    @GetMapping("{bookId}")
+    public ResponseEntity<BookInfo> getCartBook(@PathVariable Long bookId,
+                                                @RequestHeader("X-Member-ID") Long userId) {
+        BookInfo book = memberCartService.getCartItem(userId, bookId);
+        return ResponseEntity.ok().body(book);
+    }
+
+    //     장바구니 일부 삭제
+    @DeleteMapping("/{bookId}")
+    public ResponseEntity<Void> deleteCartBook(@PathVariable Long bookId,
+                                               @RequestHeader("X-Member-ID") Long userId) {
+        memberCartService.removeCartItem(userId, bookId);
+        return ResponseEntity.ok().build();
+    }
+
+    // 장바구니 전체 삭제
+    @DeleteMapping
+    public ResponseEntity<Void> deleteCartAll(@RequestHeader("X-Member-ID") Long userId) {
+        memberCartService.removeCartAllItems(userId);
+        return ResponseEntity.ok().build();
+    }
+
+}
