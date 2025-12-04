297366cbd11d265940cbaf0b4d93458368b8f4e0
hetgwi01 <hetgwi03@gmail.com>
2025-12-04 16:47:53 +0900

    병합중 변경사항

--- src/main/java/com/nhnacademy/order_payments/controller/OrderResultController.java
+++ src/main/java/com/nhnacademy/order_payments/controller/OrderResultController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/orders")
-public class OderResultController {
+public class OrderResultController {
 
     private final OrderResultService orderResultService;
 
